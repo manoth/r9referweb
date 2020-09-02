645f1e9a18461f9f0904d9016ab30dd47f8c6926
Manoth Klangphila <manoth@Manoths-MacBook-Pro.local>
2020-09-02 13:36:54 +0700

    Theme Admin-LTE 2.4.18

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SigninComponent } from './pages/signin/signin.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { ContentsComponent } from './pages/contents/contents.component';
+import { HomeComponent } from './pages/contents/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', redirectTo: 'singin', pathMatch: 'full' },
+  { path: 'singin', component: SigninComponent },
+  {
+    path: '', component: ContentsComponent,
+    children: [
+      { path: '', component: HomeComponent }
+    ]
+  },
+  { path: '**', component: PageNotFoundComponent, pathMatch: 'full' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { SidebarComponent } from './layouts/sidebar/sidebar.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { ControlSidebarComponent } from './layouts/control-sidebar/control-sidebar.component';
+import { SigninComponent } from './pages/signin/signin.component';
+import { ContentsComponent } from './pages/contents/contents.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { HomeComponent } from './pages/contents/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+    ControlSidebarComponent,
+    SigninComponent,
+    ContentsComponent,
+    PageNotFoundComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
